01a6c13db3e943a68e9a4a03a1c2e10b481418b0
Level2 DoorBuzz <sysops@syn2cat.lu>
2016-03-10 20:25:13 +0100

    shutdown utton now writes some syslog

--- shutdownbutton.py
+++ shutdownbutton.py
@@ -3,15 +3,18 @@ import RPi.GPIO as GPIO
 import time
 import os
 
+
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(buttonpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
   print "waiting..."
+  os.system('logger shutdownbutton waiting for press...')
   try:  
     GPIO.wait_for_edge(buttonpin, GPIO.FALLING)  
     print('Button Pressed')
+    os.system('logger shutdownbutton pressen. bye bye')
     os.system("shutdown now -h")
   except KeyboardInterrupt:  
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
